93225845719065715e3b4ce42da6dc335712ae5b
Organique0 <grabnar.luka@gmail.com>
2022-09-12 00:52:51 +0200

    to be continued...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupsPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigatin from "./components/layout/MainNavigation";
+
 function App() {
-  return <div>dan</div>;
+  return (
+    <div>
+      <MainNavigatin />
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />}></Route>
+        <Route path="/new-meetup" element={<NewMeetupsPage />}></Route>
+        <Route path="/favorites" element={<FavoritesPage />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
